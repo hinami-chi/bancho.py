355d6a3c00120a04288ece177e8ea00c6d1845b1
hinami-chi <akirachi666@gmail.com>
2023-06-06 20:48:18 -0400

    Stats info en mayúsculas (osu!direct)

--- app/api/domains/osu.py
+++ app/api/domains/osu.py
@@ -443,7 +443,7 @@ DIRECT_SET_INFO_FMTSTR = (
 
 DIRECT_MAP_INFO_FMTSTR = (
     "[{DifficultyRating:.2f}⭐] {DiffName} "
-    "{{cs: {CS} / od: {OD} / ar: {AR} / hp: {HP}}}@{Mode}"
+    "{{CS: {CS} / OD: {OD} / AR: {AR} / HP: {HP}}}@{Mode}"
 )
 
 
